249ab30f4c5450c9b0551b96818d0848ec465c80
Kvazikot <vsbaranov83@gmail.com>
2021-07-30 22:09:34 +0300

    add  solution for 3d coordinates

--- Assets/Scripts/TunelSegment.cs
+++ Assets/Scripts/TunelSegment.cs
@@ -116,18 +116,23 @@ public class TunelSegment : MonoBehaviour
         //set x(0) and y(0)
         float x0 = tA.position.x;
         float y0 = tA.position.z;
+        float z0 = Mathf.Max(1e-5f, tA.position.y);
 
         //set x(1) and y(1)
         float x1 = tB.position.x;
         float y1 = tB.position.z;
+        float z1 = Mathf.Max(1e-5f, tB.position.y);
 
         // set x'(0) and y'(0)
         float dUx = 1f / (x1 - x0);
         float dUy = 1f / (y1 - y0);
+        float dUz = 1f / (z1 - z0);
         float xx0 = t0.x * dUx;
         float yy0 = t0.z * dUy;
+        float zz0 = t0.y * dUz;
         float xx1 = t1.x * dUx;
         float yy1 = t1.z * dUy;
+        float zz1 = t1.y * dUz;
 
         Matrix4x4 solution = new Matrix4x4();
 
@@ -138,34 +143,44 @@ public class TunelSegment : MonoBehaviour
         {0, 1, 2, 3} });
 
         Matrix<double> v = DenseMatrix.OfArray(new double[,] {
-            {x0, y0},
-            {x1, y1},
-            {xx0, yy0},
-            {xx1, yy1}});
+            {x0, y0, z0},
+            {x1, y1, z1},
+            {xx0, yy0, zz0},
+            {xx1, yy1, zz1}});
 
         var y = A.Solve(v);
+
         //Debug.Log($"y={ y }");
         solution[0, 0] = (float)y[0, 0];
         solution[0, 1] = (float)y[0, 1];
+        solution[0, 2] = (float)y[0, 2];
         solution[1, 0] = (float)y[1, 0];
         solution[1, 1] = (float)y[1, 1];
+        solution[1, 2] = (float)y[1, 2];
         solution[2, 0] = (float)y[2, 0];
         solution[2, 1] = (float)y[2, 1];
+        solution[2, 2] = (float)y[2, 2];
         solution[3, 0] = (float)y[3, 0];
         solution[3, 1] = (float)y[3, 1];
+        solution[3, 2] = (float)y[3, 2];
 
         float U = 0;         //parameter U lies in interval [0,1]
         float ax, bx, cx, dx;
         float ay, by, cy, dy;
+        float az, bz, cz, dz;
 
         ax = solution[0, 0];
         ay = solution[0, 1];
+        az = solution[0, 2];
         bx = solution[1, 0];
         by = solution[1, 1];
+        bz = solution[1, 2];
         cx = solution[2, 0];
         cy = solution[2, 1];
+        cz = solution[2, 2];
         dx = solution[3, 0];
         dy = solution[3, 1];
+        dz = solution[3, 2];
 
         Vector3 p0 = tA.position, p = p0;
         float ustep = 1f / resolution;
@@ -175,6 +190,7 @@ public class TunelSegment : MonoBehaviour
             //when add additional dimension for 3D case
             p.x = ax + bx * U + cx * U * U + U * U * U * dx;
             p.z = ay + by * U + cy * U * U + U * U * U * dy;
+            p.y = az + bz * U + cz * U * U + U * U * U * dz;
             U += ustep;
             p0 = p;
             points[i] = p0;
